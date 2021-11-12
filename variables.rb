fa65b83574081776c3dbb636855e3bdf7bdc3841
Joseph Galvin <jwgalvin@gmail.com>
2021-11-12 09:22:04 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,11 @@
+animal = "cat"
+is_cuddly = " true"
+animal = "ferret"
+num_8 = 5
+num_3 = 8
+quotient = num_3 / num_8
+favorite_films = Array.new()
+favorite_films = ["dune", "batman", "elysium"]
+favorite_films.shift
+count = animal.size
+print count
