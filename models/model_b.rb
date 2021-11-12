b477fa4e5005510f9b9e97a75a0a340c228edc62
Joseph Galvin <jwgalvin@gmail.com>
2021-11-12 09:49:49 -0700

    created house hash

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+house = {
+  :address => "123 real street",
+  :lot_size => "3000 sqft",
+  :number_of_bedrooms => 3,
+  :number_of_baths => 2.5,
+  :finished_basment => false,
+  :remodeled_10_years => true,
+  :nearby_business => ["grocery", "school", "butcher"],
+  :access => ["roads", "trails", "lite rail"]
+}
+puts house
