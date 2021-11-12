cc7398cd6653376881b8b79a91db5d566320eb4a
Joseph Galvin <jwgalvin@gmail.com>
2021-11-12 09:49:23 -0700

    created laptop hash

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+laptop =
+{
+:Work_laptop => true,
+:Secrets => false,
+:Name => "worktop",
+:Journal => "classnotes",
+:Battery_Life => 5,
+:Programs_active_limit => 20,
+:random_array => [ "thoughts", "hamburger", 52, true],
+:second_array => [ 34, "hunger", false, "mechanical"]
+}
+puts laptop
