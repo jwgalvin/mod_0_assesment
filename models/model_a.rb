f749fa6f519babeef2fe0ce74d0d8b90bd8381a1
Joseph Galvin <jwgalvin@gmail.com>
2021-11-15 15:40:30 -0700

    update to snake_case for variables in hash

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,11 +1,11 @@
 laptop =
 {
 :Work_laptop => true,
-:Secrets => false,
-:Name => "worktop",
-:Journal => "classnotes",
-:Battery_Life => 5,
-:Programs_active_limit => 20,
+:secrets => false,
+:name => "worktop",
+:journal => "classnotes",
+:battery_Life => 5,
+:programs_active_limit => 20,
 :random_array => [ "thoughts", "hamburger", 52, true],
 :second_array => [ 34, "hunger", false, "mechanical"]
 }
